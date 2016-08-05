2e3be64d72a22fa0eba4d2c54c4748c8b256d9fb
indosm <indosm@daum.net>
2016-08-05 19:59:00 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,3 @@
-// This is an example program from the website www.microhowto.info
-// © 2012 Graham Shaw
-// Copying and distribution of this software, with or without modification,
-// is permitted in any medium without royalty.
-// This software is offered as-is, without any warranty.
-
-// Purpose: to construct an ARP request and write it to an Ethernet interface
-// using libpcap.
-//
-// See: "Send an arbitrary Ethernet frame using libpcap"
-// http://www.microhowto.info/howto/send_an_arbitrary_ethernet_frame_using_libpcap.html
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
